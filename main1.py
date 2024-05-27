d8f6020ead3fecb3bc90abbfa4570d552f13fa7a
MilaDi2023 <133892507+MilaDi2023@users.noreply.github.com>
2024-05-27 23:34:33 +0300

    Code added

--- main1.py
+++ main1.py
@@ -1,1 +1,19 @@
 import pandas as pd
+
+
+# СПОСОБ 1. СОЗДАНИЕ SERIES
+data =[1, 2, 3, 4, 5] # Создаём список
+series = pd.Series(data) # Пересохраняем список в другую переменную
+print(series) # Вывод индексированного списка в виде одномерного массива
+
+# СПОСОБ 2. СОЗДАНИЕ DATA FRAME
+# Сначала созадём словарь
+data = {
+    "Name" : ["Alice", "Bob", "Roma", "Anna"],
+    "Age": [23, 45, 17, 24],
+    "City": ["New York", "Los Angeles", "Chicago", "Moscow"]
+}
+
+# Затем преобразовываем его в таблицу
+df = pd.DataFrame(data)
+print(df) # Выводим данные в виде таблицы
